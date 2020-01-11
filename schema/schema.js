6a89e480846da08501c32414073ced423358b362
Kanuty <bartosz.dudek@netins.it>
2020-01-11 14:02:43 +0100

    node JS has been instaled. First Space Marines data have been incorporated into project

--- schema/schema.js
+++ schema/schema.js
@@ -1,35 +1,55 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 
 const {
   GraphQLObjectType,
+  GraphQLBoolean,
   GraphQLInt,
   GraphQLString,
   GraphQLSchema,
 } = graphql;
 
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 },
-];
-
-const UserType = new GraphQLObjectType ({
-  name: 'User',
+const MarinesType = new GraphQLObjectType ({
+  name: 'Marines',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt },
+    chapter: { type: GraphQLString },
+    chapterId: { type: GraphQLString },
+    rank: { type: GraphQLString },
+    company: { type: GraphQLInt },
+    squad: { type: GraphQLInt },
+    veterancy: { type: GraphQLBoolean },
+    specialisation: { type: GraphQLString },
+    primaris: { type: GraphQLBoolean },
+    alive: { type: GraphQLBoolean },
+  }
+})
+
+const ChapterType = new GraphQLObjectType ({
+  name: 'Chapters',
+  fields: {
+    id: { type: GraphQLString },
+    originalName: { type: GraphQLString },
+    currentName: { type: GraphQLString },
+    warcry: { type: GraphQLString },
+    founding: { type: GraphQLString },
+    successorsOf: { type: GraphQLString },
+    successorChapters: { type: GraphQLString },
+    homeworld: { type: GraphQLString },
+    allegiance: { type: GraphQLString },
   }
 })
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    user: {
-      type: UserType,
+    marine: {
+      type: MarinesType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-          return _.find(users, { id: args.id });
+          return axios.get(`http://localhost:3000/marines/${args.id}`)
+            .then(resp => resp.data);
       }
     }
   }
