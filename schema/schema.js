1bf5f7ebf0b76fe69ef3291e81bcff3d0f5905dc
Kanuty <bartosz.dudek@netins.it>
2020-01-11 20:02:26 +0100

    mutations -> add, delete and edit(patch) have been added to project

--- schema/schema.js
+++ schema/schema.js
@@ -5,30 +5,15 @@ const {
   GraphQLObjectType,
   GraphQLBoolean,
   GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
   GraphQLString,
   GraphQLSchema,
 } = graphql;
 
-const MarinesType = new GraphQLObjectType ({
-  name: 'Marines',
-  fields: {
-    id: { type: GraphQLString },
-    firstName: { type: GraphQLString },
-    chapter: { type: GraphQLString },
-    chapterId: { type: GraphQLString },
-    rank: { type: GraphQLString },
-    company: { type: GraphQLInt },
-    squad: { type: GraphQLInt },
-    veterancy: { type: GraphQLBoolean },
-    specialisation: { type: GraphQLString },
-    primaris: { type: GraphQLBoolean },
-    alive: { type: GraphQLBoolean },
-  }
-})
-
 const ChapterType = new GraphQLObjectType ({
   name: 'Chapters',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     originalName: { type: GraphQLString },
     currentName: { type: GraphQLString },
@@ -38,7 +23,37 @@ const ChapterType = new GraphQLObjectType ({
     successorChapters: { type: GraphQLString },
     homeworld: { type: GraphQLString },
     allegiance: { type: GraphQLString },
-  }
+    marines: {
+      type: new GraphQLList(MarinesType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/chapters/${parentValue.id}/marines`)
+        .then(res => res.data);
+      }
+    },
+  })
+})
+
+const MarinesType = new GraphQLObjectType ({
+  name: 'Marines',
+  fields: () => ({
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    chapter: { 
+      type: ChapterType ,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/chapters/${parentValue.chapterId}`)
+          .then(res => res.data);
+      }
+    },
+    chapterId: { type: GraphQLString },
+    rank: { type: GraphQLString },
+    company: { type: GraphQLInt },
+    squad: { type: GraphQLInt },
+    veterancy: { type: GraphQLBoolean },
+    specialisation: { type: GraphQLString },
+    primaris: { type: GraphQLBoolean },
+    alive: { type: GraphQLBoolean },
+  })
 })
 
 const RootQuery = new GraphQLObjectType({
@@ -51,10 +66,58 @@ const RootQuery = new GraphQLObjectType({
           return axios.get(`http://localhost:3000/marines/${args.id}`)
             .then(resp => resp.data);
       }
+    },
+    chapter: {
+      type: ChapterType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+          return axios.get(`http://localhost:3000/chapters/${args.id}`)
+            .then(resp => resp.data);
+      }
+    }
+  }
+});
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addMarine: {
+      type: MarinesType,
+      args: {
+        id: { type: GraphQLString },
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parentValue, { firstName }) {
+        return axios.post('http://localhost:3000/marines', { firstName })
+          .then(res => res.data);
+      }
+    },
+    deleteMarine: {
+      type: MarinesType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parentValue, { id }) {
+        return axios.delete(`http://localhost:3000/marines/${id}`)
+          .then(res => res.data);
+      }
+    },
+    editMarine: {
+      type: MarinesType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        rank: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`http://localhost:3000/marines/${args.id}`, args)
+          .then(res => res.data);
+      }
     }
   }
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation,
 });
